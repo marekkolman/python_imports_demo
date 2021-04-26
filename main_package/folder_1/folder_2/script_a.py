cce10dce58fffe560e7b23cd1c69b404c4169663
Marek Kolman <marek.kolman@gmail.com>
2021-04-26 20:25:17 +0200

    case_1

--- main_package/folder_1/folder_2/script_a.py
+++ main_package/folder_1/folder_2/script_a.py
@@ -1,2 +1,4 @@
-def hw_script_a():
-    print('hello world in script a')
+from main_package.folder_3 import script_c # this is absolute import
+
+def test_imported_method(): # use the imported method from script_c
+    script_c.hw_script_c()
---
+++ testing_file.py
@@ -0,0 +1,3 @@
+import main_package.folder_1.folder_2.script_a as script_a
+
+script_a.test_imported_method()
